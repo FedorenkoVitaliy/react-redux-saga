ddf76eda7b282f3ddcf05677641636746168406f
Виталий Федоренко <fedvetal2012@gmail.com>
2020-11-10 23:24:46 +0200

    feat: thunk async func

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Post from "./Post";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchPosts} from "../redux/actions";
+import {Loader} from "./loader";
 
-const FetchedPosts = ({ posts }) => {
-  if(!posts.length){
-    return <button className="btn btn-primary">Загрузить</button>
+const FetchedPosts = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector(state => state.posts.fetchedPosts);
+  const loading = useSelector(state => state.app.loading);
+  console.log(loading)
+  if(loading){
+    return <Loader/>;
   }
-  return posts.map((post, idx) => <Post post={post} key={idx}/>);
+  if(!posts.length){
+    return (
+      <button
+        className="btn btn-primary"
+        onClick={() => dispatch(fetchPosts())}
+      >
+        Загрузить
+      </button>
+    )
+  }
+  return posts.map((post) => <Post post={post} key={post.id}/>);
 }
 export default FetchedPosts;
